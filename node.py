a007db9cbd5a0cd6dd6aebd65602e162252b932b
ChrisPapad <sdi1400145@di.uoa.gr>
2020-07-08 13:51:08 +0300

    Added a test variable to emulate network partition

--- node.py
+++ node.py
@@ -1,4 +1,6 @@
 import psycopg2
+import random
+import time
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
@@ -11,6 +13,7 @@ class Node:
         self.con = psycopg2.connect(dbname='postgres',
         user=user_name, host='localhost',
         password=password)
+        self.db_name = db_name
 
         self.con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
@@ -20,7 +23,7 @@ class Node:
         sql.Identifier(db_name))
         )
 
-        self.con = psycopg2.connect(dbname=db_name,
+        self.con = psycopg2.connect(dbname=self.db_name,
         user=user_name, host='localhost',
         password=password)
 
@@ -28,23 +31,36 @@ class Node:
 
         self.cur = self.con.cursor()
 
-        self.cur.execute("CREATE TABLE Votes (id serial PRIMARY KEY, vote integer, Name varchar, Surname varchar);")
+        self.cur.execute("CREATE TABLE Votes (id serial PRIMARY KEY, Vote integer, Vote_id bigint, Name varchar, Surname varchar);")
 
         self.con.commit()
 
-    def insert(self,vote,name,surname):
-        postgres_insert_query = """ INSERT INTO Votes (vote, Name, Surname) VALUES (%s,%s,%s)"""
-        record_to_insert = (vote, name, surname)
+    def insert(self,vote,vote_id,name,surname,test=False):
+        postgres_insert_query = """ INSERT INTO Votes (Vote, Vote_id, Name, Surname) VALUES (%s,%s,%s,%s)"""
+        record_to_insert = (vote, vote_id, name, surname)
+
+        #randomly "drop" the connection if test=True
+        if(test):
+            drop = bool(random.getrandbits(1))
+            if(drop):
+                sleep_time = random.uniform(200, 3000)
+                time.sleep(sleep_time/1000)
+                print("Node %s: Vote not saved (Timeout)" % self.db_name)
+                return -2          #code for timeout
+
+
         try:
             self.cur.execute(postgres_insert_query, record_to_insert)
             self.con.commit()
+            return 1
         except:
             return -1
 
-    def getTotalVotes(self);
+    def getTotalVotes(self):
 
         self.cur.execute("SELECT COUNT(*) FROM Votes;")
-        return self.cur.fetchall()
+        res=self.cur.fetchall()
+        return res[0][0]
 
 
 
