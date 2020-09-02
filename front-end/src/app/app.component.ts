9e16a1505a5d5cbaafa7aa766e1dee3dc61b8f2c
Marios Papamichalopoulos <papamichalopoulos.m@gmail.com>
2020-09-03 00:41:57 +0300

    html index added vote view

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { faRepublican, faDemocrat, faFire, faVoteYea } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent { 
+  faRepublican = faRepublican;
+  faDemocrat = faDemocrat;
+  faFire = faFire;
+  faVoteYea = faVoteYea;
 }
